8ed9b8f8c10d088e39806bd41d85c44d800ecb07
sefikanurtopuz <sefikanurtopuz@gmail.com>
2023-02-21 11:17:59 +0300

    hataların düzeltilmesi gerekiyor

--- Program.cs
+++ Program.cs
@@ -11,8 +11,14 @@ namespace _1_den_1_milyona_kadar_sayiları_7_ye_bolme
         static void Main(string[] args)
         {
             int sayac = 0;
+
+            //
             Console.WriteLine("Lütfen 1000000 Kadar Bir Sayı Giriniz");
             Console.WriteLine(ConvertToInt32(Console.ReadLine());
+            //
+
+            //yukardaki iki satır neden var, olmalı mı???
+
             for (int i = 1; i <= 1000000; i++)
             {
                 if (i % 7 == 0)
