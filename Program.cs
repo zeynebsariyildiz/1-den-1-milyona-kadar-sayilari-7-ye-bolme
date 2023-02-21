900ea5488901a0be286684cfcb2d3ed5810a14d1
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-02-21 11:06:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_den_1_milyona_kadar_sayiları_7_ye_bolme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+            Console.WriteLine("Lütfen 1000000 Kadar Bir Sayı Giriniz");
+            Console.WriteLine(ConvertToInt32(Console.ReadLine());
+            for (int i = 1; i <= 1000000; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                    sayac++;
+                }
+            }
+            Console.WriteLine("7'ye tam bölünen sayı adeti={0}", sayac);
+            Console.ReadKey();
+        }
+    }
+}
